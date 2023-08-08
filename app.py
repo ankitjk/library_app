6b21e8e147a888afcb44087908d50d208b0a38a8
Ankit Kulshrestha <ankit@Ankits-MacBook-Pro.local>
2023-08-08 11:33:56 -0500

    create book page added

--- app.py
+++ app.py
@@ -78,3 +78,17 @@ with app.app_context():
 def index():
     books = Book.query.all()
     return render_template('index.html', books=books)
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        isbn = request.form['isbn']
+        title = request.form['title']
+        author = request.form['author']
+        year_published = request.form['year_published']
+        publisher = request.form['publisher']
+        book = Book(isbn=isbn, title=title, author=author, year_published=year_published, publisher=publisher)
+        db.session.add(book)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('create.html')
