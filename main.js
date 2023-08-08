8c9b92b5d23e3c3ecaa5319c77af860c7671242d
Xaivex <132638283+Xaivex@users.noreply.github.com>
2023-08-08 08:49:38 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,25 +6,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const highToLowButton = document.getElementById('btn-high-to-low');
 
     // Add event listeners and implement the logic for filtering and displaying books
-    // You can use Fetch API or other AJAX methods to retrieve and display book data
 
-    // Example event listener for search input
+    // Event listener for search input
     searchInput.addEventListener('input', () => {
         const searchText = searchInput.value.trim();
         // Implement search functionality here
     });
 
-    // Example event listener for alphabetical sorting button
+    // Event listener for alphabetical sorting button
     alphaButton.addEventListener('click', () => {
         // Implement alphabetical sorting logic here
     });
 
-    // Example event listener for ratings low to high button
+    // Event listener for ratings low to high button
     lowToHighButton.addEventListener('click', () => {
         // Implement ratings low to high sorting logic here
     });
 
-    // Example event listener for ratings high to low button
+    // Event listener for ratings high to low button
     highToLowButton.addEventListener('click', () => {
         // Implement ratings high to low sorting logic here
     });
