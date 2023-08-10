cba81807a9eaded2bb5639ec607a94c63d0494d3
Ankit Kulshrestha <ankit@Ankits-MacBook-Pro.local>
2023-08-10 15:49:29 -0500

    update run.py

--- library/models.py
+++ library/models.py
@@ -6,12 +6,15 @@ class Book(db.Model):
     author = db.Column(db.String(256), nullable=False)
     year_published = db.Column(db.Integer, nullable=False)
     publisher = db.Column(db.String(256))
+    borrowed = db.Column(db.Boolean, nullable=False)
     
 class Rating(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
-    isbn = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), db.ForeignKey('user.username'), primary_key=True)
+    isbn = db.Column(db.Integer, db.ForeignKey('book.isbn'), primary_key=True)
     rating = db.Column(db.Integer, nullable=False)
 
 class User(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
-    age = db.Column(db.Integer, nullable=True)
+    username = db.Column(db.String(50), primary_key=True)
+    password = db.Column(db.String(20), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(256), nullable=False)
--- run.py
+++ run.py
@@ -1,4 +1,25 @@
-from library import app
+from library import app, db
+from library.models import *
+import csv
 
 if __name__ == '__main__':
+    # create tables
+    with app.app_context():
+        # db.drop_all()
+        db.create_all()
+        # with open("BookData.csv", mode='r') as f:
+        #     csvFile = csv.reader(f)
+        #     i = 0
+        #     for row in csvFile:
+        #         if row[0] == "ISBN":
+        #             continue
+        #         if i > 100:
+        #             break
+        #         try:
+        #             book = Book(isbn=int(row[0]), title=row[1], author=row[2], year_published=int(row[3]), publisher=row[4], borrowed=False)
+        #             db.session.add(book)
+        #         except:
+        #             continue
+        #         i += 1
+        db.session.commit()
     app.run()
